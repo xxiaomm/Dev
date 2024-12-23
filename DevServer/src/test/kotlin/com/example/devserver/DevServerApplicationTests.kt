ed93176579dfd5b3c9391bfe491f5e6430be006f
xxiaomm <xm2074@nyu.edu>
2024-12-23 11:36:26 -0500

    add backend init

---
+++ DevServer/src/test/kotlin/com/example/devserver/DevServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.devserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
